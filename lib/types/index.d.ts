80295c2abe9127e2b1028ea5bad4c7bc85940151
Jerry Schneider <schnej7@gmail.com>
2025-09-02 20:10:23 -0400

    Update lib + version

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -2,17 +2,21 @@ type SubclassRegistry = {
     forward: Map<string, BetterEnum>;
     reverse: Map<BetterEnum, string>;
 };
+type EnumConstructorSignature<T extends BetterEnum> = new (...args: any[]) => T;
+type EnumConstructor<T extends BetterEnum> = EnumConstructorSignature<T> & {
+    getSubclassRegistry(): SubclassRegistry | undefined;
+};
 /**
  * Class decorator that automatically registers enum instances after class definition is complete.
  * This eliminates the need for queueMicrotask in the constructor.
  */
-export declare function RegisterEnum<T extends new (...args: any[]) => BetterEnum>(constructor: T): T;
+export declare function RegisterEnum<T extends EnumConstructorSignature<BetterEnum>>(constructor: T): T;
 export default abstract class BetterEnum {
     private static _registry;
     toString(): string;
-    static fromString<T extends typeof BetterEnum>(this: T, key: string): InstanceType<T> | undefined;
-    static values<T extends typeof BetterEnum>(this: T): InstanceType<T>[];
-    private static getSubclassRegistry;
+    static fromString<T extends BetterEnum>(this: EnumConstructor<T>, key: string): T | undefined;
+    static values<T extends BetterEnum>(this: EnumConstructor<T>): readonly T[];
+    static getSubclassRegistry(): SubclassRegistry | undefined;
     static initSubclassRegistry(subclass: typeof BetterEnum): SubclassRegistry;
 }
 export {};
